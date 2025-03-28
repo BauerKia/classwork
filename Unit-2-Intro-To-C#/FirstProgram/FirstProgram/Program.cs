4997d1b0d84e2b8582c934c3e66e8bb2dadae40b
Kiana Bauer <bauerkia@msu.edu>
2025-03-27 21:57:51 -0400

    firstporjects

---
+++ Unit-2-Intro-To-C#/FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace FirstProgram;
+// This program will ask for three numbers
+// add them together and display
+
+/* *****Programming is coming up with the solution****
+* Think like a human first! Coder Second!
+* Coding is writing that solution into a language
+*/
+
+//Programming Solution - Human Thinking
+//Identify the detaileds steps
+
+//Ask for the numbers -> one at a time
+// -> Write down each number as its given
+// Add the numbers together -> create a sum/total
+// Say the total (display)
+
+
+// Now that we have the steps in our solution
+//Identify any data you need for the steps
+
+// 1. A place to write down each number -> A variable in a program
+// 2. A place to hold the sum/total -> A variable in a program
+// 3. A way to ask for the numbers -> A method in a program (object.method() )
+// 4. A way to receive the numbers -> A method in a program (object.method() )
+// A way to report the sum/total -> A method in a program (object.method() )
+
+// Place the steps in the order we need to solve the program and add more details if necessary 
+/*
+ * 1. Ask for the numbers one at a time
+ *          a.Tell whoever is giving us the numbers to give me the first number
+ *          b. Receive the first number from the sender
+ *          c. Write down the first number
+ * Repeat two more times
+ */
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Variable names C# are usually in camelCase
+        // Its always good to initialize variables when defing them so you know what is in them
+        
+        Console.WriteLine("--- Starting program ---");
+            //Define three numbers
+            int num1 = 0;
+            int num2 = 0;
+            int num3 = 0;
+            
+            //Define a var to hold the bum of the numbers
+            int sum = 0;
+            
+            //Ask for the nums one at a time using the C# Console object
+            // which represents the keyboard and screen
+            
+            Console.WriteLine("Give me a number: "); //Asking for the number
+            string aLine;
+            aLine = Console.ReadLine(); // Get a line from the keyboard
+          //We need an int value to store our numbers
+          // Console.ReadLine() only returns a string
+          // So we need to convert the string from Console.ReadLine() to an int
+          num1 = int.Parse(aLine);
+          
+          // Verify that I got the data expected
+          // Display some words and the value I received
+          //Little pieces + test them!
+          
+          Console.WriteLine("You entered: " + num1);
+          Console.WriteLine("--- Ending program ---");
+    }
+}
---
+++ Unit-2-Intro-To-C#/Project1/Project1/Program.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * This is a block comment - everything between the slash-*
+ *  and *-slash is ignored
+ *
+ *  The * at the start of each line is optional
+ */
+
+// A namespace identifies a context in which a word is known
+// allows the same name to be used in different contexts to mena different things 
+namespace Project1;
+
+// everything in C# is defined in a Class
+// A Class is a group of related things
+// A Class starts with the class keyword
+class Program
+
+// {} surround blocks of related code aka code-blocks
+{ // Start of code for the class
+    // Every application has a program called Main()
+    // A method is a self-contained set code that performs a functions
+    // aka: a function or a program
+    // a method may return a value and accept data to process
+    // Method signature identifies wha the method returns it's name and what it accepts
+    //return-type name(data-it-accepts)
+    
+    //Main() is where every application starts 
+    //Every application has one and only Main() method
+    
+    //Below Main() is a method that returns void and accepts string[] called args
+    
+    //return
+    //     type name(data-it-accepts)
+    
+    static void Main(string[] args) // method signature
+    { // Start of the code for Main()
+        //This code will display what ever is between the ("") on the screen
+        
+        //Console is an object defined by C# to represent the screen
+        // An object has certain behviors
+        // A behavior is something an object can do with data
+        // Behaviors are implemented using methods
+        
+        // object.method(data) - object oriented programmming
+        
+        
+        //When you see a . after a name, the name too left of the . is probably an object
+        // When you see name() the name is a method
+        // When you see a .name , the name is usually method
+        // object.method(data-for-the-method-to-process)
+        
+        
+        Console.WriteLine("Hello, World!");
+        
+        
+        
+    } // End of code for Main()
+}
+// End of code for the Class
