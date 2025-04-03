0b07f2ec6d4807f71d8503c74fbd3c7bd6fd024e
Kiana Bauer <bauerkia@msu.edu>
2025-04-03 18:35:55 -0400

    two labs

---
+++ Unit-2-Intro-To-C#/BasicLoops/BasicLoops/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace BasicLoops;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        /*
+         * Problem 1
+         */
+        string input = "";
+        do
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Would you like to continue (y/n)?");
+             input = Console.ReadLine();
+        } while (input == "y");
+Console.WriteLine("Goodbye!");
+
+/*
+ * Problem 2
+ */
+
+Console.WriteLine("Please enter a number:");
+int inputNum = int.Parse(Console.ReadLine());
+for (int i = 0; i < inputNum; i--)
+{
+    
+}
+
+Console.WriteLine("Would you like to continue (y/n)?")
+input = Console.ReadLine();
+if (input == "y")
+{
+    
+
+}
+
+    }
+}
+
+static void printNumbers(int inputNum)
+{
+    for (int i = 0; i < inputNum; i--)
+    {
+    
+    }
+}
