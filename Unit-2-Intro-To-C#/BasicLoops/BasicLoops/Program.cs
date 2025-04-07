e92c8ba9b7a0ea6be2b238461a8e25f79346a1e7
Kiana Bauer <bauerkia@msu.edu>
2025-04-07 18:56:58 -0400

    loops

--- Unit-2-Intro-To-C#/BasicLoops/BasicLoops/Program.cs
+++ Unit-2-Intro-To-C#/BasicLoops/BasicLoops/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace BasicLoops;
+﻿using System.Diagnostics;
+using System.Numerics;
+
+namespace BasicLoops;
 
 class Program
 {
     static void Main(string[] args)
     {
-        
+
         /*
          * Problem 1
          */
@@ -13,12 +16,52 @@ class Program
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Would you like to continue (y/n)?");
-             input = Console.ReadLine();
+            input = Console.ReadLine();
         } while (input == "y");
-Console.WriteLine("Goodbye!");
+
+        Console.WriteLine("Goodbye!");
 
 /*
  * Problem 2
  */
-Console.WriteLine("Would you like to continue (y/n)?");
-input = Console.ReadLine();
+        string newInput = "";
+        do
+        {
+            Console.WriteLine("Please Enter a number:");
+            int inputNum = int.Parse(Console.ReadLine());
+         //n  Console.WriteLine(lineOfNum(inputNum));
+         int[] newArray = new int[inputNum];
+         int[] newArray2 = new int[inputNum];
+         for (int i = inputNum; i <= inputNum && i !< 0; i--)
+         {
+             newArray[i] =  i;
+             
+         }
+         Console.WriteLine($"{newArray}");
+         for (int i = 0; i <= inputNum && i !> inputNum; i++)
+         {
+             newArray2[i] = i;
+             
+         }
+         Console.WriteLine($"{newArray2}");
+            Console.WriteLine("Would you like to continue (y/n)?");
+            newInput = Console.ReadLine();
+        } while (newInput == "y");
+
+        Console.WriteLine("Goodbye!");
+        
+    }
+
+    
+    List<int> numList = new List<int>();
+        for (int i = 0; i < inputNum; i++)
+    {
+        numList.Add(i);
+    }
+
+// You can convert it back to an array if you would like to
+    int[] finalArray = numList.ToArray();
+
+
+}
+
