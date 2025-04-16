faaa071a12d645a2f1165b954cd5485bc5509c27
Kiana Bauer <bauerkia@msu.edu>
2025-04-16 17:46:49 -0500

    update 4/16

---
+++ Unit-3-Collections/SplitLab/SplitLab/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace SplitLab;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        string input = "";
+        //Problem 1
+        do
+        {
+            Console.WriteLine("Please enter a sentence: ");
+            input = Console.ReadLine();
+
+            string[] split = input.Split(' ');
+
+            foreach (var word in split)
+            {
+                Console.WriteLine(word);
+            }
+            Console.WriteLine("Do you want to continue? (y/n)");
+            input = Console.ReadLine();
+        } while (input == "y");
+        
+        Console.WriteLine("Goodbye!");
+        
+        
+        // Problem 2
+        string add = "";
+        do
+        {
+            Console.WriteLine("Please enter some text: ");
+            input = Console.ReadLine();
+            
+            add = add + " " + input;
+                
+            Console.WriteLine($"You have entered: {add}");
+                
+            Console.WriteLine("Do you want to continue? (y/n)");
+            input = Console.ReadLine();
+        } while (input == "y");
+        
+        Console.WriteLine("Goodbye!");
+    }
+}
